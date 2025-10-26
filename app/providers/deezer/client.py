c26ba557782a62d4f36bf9a95b79b7b87a5366fa
shinzo13 <verysafeaccount@proton.me>
2025-10-26 12:35:35 +0100

    feat(deezer): API requests realization & some changes

--- app/providers/deezer/client.py
+++ app/providers/deezer/client.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 from aiohttp import ClientSession
 
-from app.providers.deezer.utils.get_arl import get_arl
-from app.providers.deezer import enums
+from app.providers.deezer.models import DeezerTrack
+from app.providers.deezer.utils import get_arl
+from app.providers.deezer.enums import DeezerAPIMethod
 from app.providers.deezer.constants import *
 
 class DeezerClient:
@@ -27,11 +30,41 @@ class DeezerClient:
                 'arl': self.arl,
                 'comeback': '1'
             },
-            headers=headers
+            headers=HEADERS
         )
 
         options = (await (await self.session.get(
             USERDATA_URL
         )).json())['results']['USER']['OPTIONS']
         self.license_token = options['license_token']
+
+    async def _api_request(
+        self,
+        method: DeezerAPIMethod,
+        params: dict[str, Any],
+    ) -> dict:
+        async with self.session.get(
+            f"{API_URL}/{method}",
+            params=params,
+        ) as resp:
+            raw_data = await resp.json()
+            if "error" in raw_data:
+                raise # TODO separeted DeezerAPIException
+            return raw_data # TODO data processing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- app/providers/deezer/enums/api_method.py
+++ app/providers/deezer/enums/api_method.py
@@ -1,4 +1,7 @@
 from enum import StrEnum
 
 class DeezerAPIMethod(StrEnum):
-    ...
+    SEARCH_TRACKS = 'search/track'
+    SEARCH_ALBUM = 'search/album'
+    SEARCH_ARTIST = 'search/artist'
+    SEARCH_PLAYLIST = 'search/playlist'
