dde8cfb9a95a7c662522199f78722dadda1d265c
shinzo13 <verysafeaccount@proton.me>
2025-12-07 15:10:53 +0100

    feat(bot): Added constants

---
+++ app/bot/constants.py
@@ -0,0 +1,14 @@
+from app.modules.musicocean.enums import Engine, EntityType
+
+ENGINE_PREFIXES = {
+    Engine.DEEZER       : "dz",
+    Engine.SOUNDCLOUD   : "sc",
+    Engine.YOUTUBE      : "yt",
+    Engine.SPOTIFY      : "sp"
+}
+
+ENTITY_PREFIXES = {
+    EntityType.ALBUM    : "al",
+    EntityType.ARTIST   : "ar",
+    EntityType.PLAYLIST : "pl"
+}
