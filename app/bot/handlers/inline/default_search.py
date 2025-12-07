2aaf546163b14eaf2572830e5f87484efa80d226
shinzo13 <verysafeaccount@proton.me>
2025-12-07 15:14:58 +0100

    feat(bot): Added receiving user's selected engine from DB

--- app/bot/handlers/inline/default_search.py
+++ app/bot/handlers/inline/default_search.py
@@ -7,7 +7,7 @@ from app.modules.musicocean_tg import TelegramMusicOceanClient
 from app.modules.musicocean.enums.engine import Engine
 from app.bot.utils.search_results import get_track_results
 from app.config.log import get_logger
-
+from app.database.models import User as DatabaseUser # TODO costyl
 
 logger = get_logger(__name__)
 
@@ -16,10 +16,11 @@ router = Router()
 @router.inline_query(F.query!="")
 async def inline_query(
     query: InlineQuery,
+    user: DatabaseUser,
     musicocean: FromDishka[TelegramMusicOceanClient],
     track_repo: FromDishka[TrackRepository]
 ):
     logger.info(f"User #{query.from_user.id} searched for \"{query.query}\"")
-    # TODO zaglushka
-    matches = await musicocean.search_tracks(Engine.DEEZER, query.query)
-    await query.answer(await get_track_results(Engine.DEEZER, matches, track_repo))
+    # TODO inline engine selecting
+    matches = await musicocean.search_tracks(user.selected_engine, query.query)
+    await query.answer(await get_track_results(user.selected_engine, matches, track_repo))
