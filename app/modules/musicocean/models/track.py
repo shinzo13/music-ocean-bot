9b35e48e228027e3ae6c3206b948956a1e46e21d
shinzo13 <verysafeaccount@proton.me>
2025-12-09 15:30:15 +0100

    feat(musicocean): Grouped track model fields by type (for now)

--- app/modules/musicocean/models/track.py
+++ app/modules/musicocean/models/track.py
@@ -3,19 +3,25 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+# TODO!! use Track only as MO-ready class, delete in-engine fields
+
 class Track(BaseModel):
     """
     Basic Track model. Contains all the information needed to properly download a track.
     """
 
+    # Necessary
     id: int
     title: str
     artist_name: str
     # TODO album_title: str
     cover_url: str
 
-    preview_url: Optional[str] = None
-    track_token: Optional[str] = None
+    # Common
     duration: Optional[int] = None
     content: Optional[bytes] = None
     cover: Optional[bytes] = None
+
+    # Deezer
+    preview_url: Optional[str] = None
+    track_token: Optional[str] = None
