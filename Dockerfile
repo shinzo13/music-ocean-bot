ed1736b5f9c1d86dcbd350528217861dea3f27d3
shinzo13 <verysafeaccount@proton.me>
2026-03-01 15:55:52 +0100

    feat(global): Temporary removed playwright logic
    Not enough space on my VPS for 3gb docker image...

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,9 @@ WORKDIR /app
 ADD pyproject.toml uv.lock ./
 RUN uv sync --frozen --no-dev
 RUN apt-get update && \
-    uv run playwright install --with-deps firefox && \
-    rm -rf /var/lib/apt/lists/*
-
+#    uv run playwright install --with-deps firefox && \
+    rm -rf /var/lib/apt/lists/* && \
+    && uv cache clean
 
 COPY . .
 
--- app/modules/musicocean/engines/deezer/utils/get_arl.py
+++ app/modules/musicocean/engines/deezer/utils/get_arl.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from playwright.async_api import async_playwright
+#from playwright.async_api import async_playwright
 
 from app.config.log import get_logger
 from app.config.settings import settings
@@ -15,30 +15,30 @@ async def get_arl(email, password) -> str:
         # ZAGLUSHKA
         logger.info("Using saved ARL cookie (dev mode)")
         return settings.dev.arl
-
-    async with async_playwright() as p:
-        browser = await p.firefox.launch(headless=True)
-        logger.debug("Launched browser")
-        context = await browser.new_context()
-        page = await context.new_page()
-        await page.goto("https://account.deezer.com/login/")
-        await page.wait_for_load_state("networkidle")
-        logger.debug("Page loaded")
-        await page.evaluate('document.getElementById("cookie-banner-deezer")?.remove()')
-        await page.fill('input[id="email"]', email)
-        await page.fill('input[id="password"]', password)
-        await page.click('button[type="submit"]')
-        await page.wait_for_load_state("networkidle")
-        logger.debug("Submitted credentials")
-        await asyncio.sleep(1)
-        cookies = await context.cookies()
-        arl_cookie = None
-        for cookie in cookies:
-            if cookie['name'] == 'arl':
-                arl_cookie = cookie['value']
-                break
-        await browser.close()
-        if arl_cookie:
-            logger.info("Successfully fetched ARL cookie")
-            return arl_cookie
-        raise Exception("Error getting arl cookie")  # TODO exception class
+    raise
+    # async with async_playwright() as p:
+    #     browser = await p.firefox.launch(headless=True)
+    #     logger.debug("Launched browser")
+    #     context = await browser.new_context()
+    #     page = await context.new_page()
+    #     await page.goto("https://account.deezer.com/login/")
+    #     await page.wait_for_load_state("networkidle")
+    #     logger.debug("Page loaded")
+    #     await page.evaluate('document.getElementById("cookie-banner-deezer")?.remove()')
+    #     await page.fill('input[id="email"]', email)
+    #     await page.fill('input[id="password"]', password)
+    #     await page.click('button[type="submit"]')
+    #     await page.wait_for_load_state("networkidle")
+    #     logger.debug("Submitted credentials")
+    #     await asyncio.sleep(1)
+    #     cookies = await context.cookies()
+    #     arl_cookie = None
+    #     for cookie in cookies:
+    #         if cookie['name'] == 'arl':
+    #             arl_cookie = cookie['value']
+    #             break
+    #     await browser.close()
+    #     if arl_cookie:
+    #         logger.info("Successfully fetched ARL cookie")
+    #         return arl_cookie
+    #     raise Exception("Error getting arl cookie")  # TODO exception class
