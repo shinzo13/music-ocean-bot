12a57c1a0ee86863e0ad829092c3ea3c3e2e2a50
shinzo13 <verysafeaccount@proton.me>
2026-05-22 00:33:25 +0200

    feat(bot): remove deprecated pending tracks logic

--- app/bot/handlers/process_track/__init__.py
+++ app/bot/handlers/process_track/__init__.py
@@ -1,5 +1,4 @@
 from .download_all import router as download_all_router
 from .inline_chosen import router as process_track_router
-from .pending_tracks import router as pending_tracks_router
 
-routers = [process_track_router, download_all_router, pending_tracks_router]
+routers = [process_track_router, download_all_router]
