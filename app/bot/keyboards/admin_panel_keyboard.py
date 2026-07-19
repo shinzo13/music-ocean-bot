51b81c2bdba2a8466ae415e2e305ab4ed52f8ab9
shinzo13 <verysafeaccount@proton.me>
2026-07-19 17:18:20 +0200

    feat(bot): update admin panel emojis & keyboard markup

--- app/bot/keyboards/admin_panel_keyboard.py
+++ app/bot/keyboards/admin_panel_keyboard.py
@@ -3,34 +3,40 @@ from aiogram_i18n.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from app.bot.callbacks.admin_panel_callback import AdminPanelCallback, AdminPanelPath
 from app.bot.callbacks.main_menu_callback import MainMenuCallback, MainMenuPath
-from app.bot.constants import DATABASE_EMOJI_ID, BACK_EMOJI_ID, MAILING_EMOJI_ID
+from app.bot.constants import DATABASE_EMOJI_ID, BACK_EMOJI_ID, MAILING_EMOJI_ID, STATS_EMOJI_ID, BAN_EMOJI_ID
 
 
 def admin_panel_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[  # noqa
-        [InlineKeyboardButton(
-            text=LazyProxy('btn-export-users'),
-            callback_data=AdminPanelCallback(path=AdminPanelPath.EXPORT_USERS).pack(),
-            icon_custom_emoji_id=DATABASE_EMOJI_ID
-        )],
-        [InlineKeyboardButton(
-            text=LazyProxy('btn-mailing'),
-            callback_data=AdminPanelCallback(path=AdminPanelPath.MAILING).pack(),
-            icon_custom_emoji_id=MAILING_EMOJI_ID
-        )],
-        [InlineKeyboardButton(
-            text='ban user',
-            callback_data=AdminPanelCallback(path=AdminPanelPath.BAN_USER).pack(),
-            icon_custom_emoji_id=DATABASE_EMOJI_ID
-        )],
-        [InlineKeyboardButton(
-            text='usage stats',
-            callback_data=AdminPanelCallback(path=AdminPanelPath.USAGE_STATS).pack(),
-            icon_custom_emoji_id=DATABASE_EMOJI_ID
-        )],
-        [InlineKeyboardButton(
-            text=LazyProxy('btn-back'),
-            callback_data=MainMenuCallback(path=MainMenuPath.SELF).pack(),
-            icon_custom_emoji_id=BACK_EMOJI_ID
-        )]
+        [
+            InlineKeyboardButton(
+                text=LazyProxy('btn-export-users'),
+                 callback_data=AdminPanelCallback(path=AdminPanelPath.EXPORT_USERS).pack(),
+                icon_custom_emoji_id=DATABASE_EMOJI_ID
+            ),
+            InlineKeyboardButton(
+                text=LazyProxy('btn-mailing'),
+                callback_data=AdminPanelCallback(path=AdminPanelPath.MAILING).pack(),
+                icon_custom_emoji_id=MAILING_EMOJI_ID
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='ban user',
+                callback_data=AdminPanelCallback(path=AdminPanelPath.BAN_USER).pack(),
+                icon_custom_emoji_id=BAN_EMOJI_ID
+            ),
+            InlineKeyboardButton(
+                text='usage stats',
+                callback_data=AdminPanelCallback(path=AdminPanelPath.USAGE_STATS).pack(),
+                icon_custom_emoji_id=STATS_EMOJI_ID
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=LazyProxy('btn-back'),
+                callback_data=MainMenuCallback(path=MainMenuPath.SELF).pack(),
+                icon_custom_emoji_id=BACK_EMOJI_ID
+            )
+        ]
     ])
